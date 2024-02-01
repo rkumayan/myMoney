5120abd83c0d8f8fcd232577592e5c181069e7c2
rkumayan <kumayanrahul01@gmail.com>
2024-02-01 18:23:50 +0530

    added navbar component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-
+        <Navbar />
         <Routes>
           <Route path = "/" element = {<Home/>}></Route>
           <Route path = "/login" element = {<Login/>}></Route>
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import styles from './Navbar.module.css';
+import { Link } from 'react-router-dom';
+const Navbar = () => {
+    return ( 
+        <nav className={ styles.navbar}>
+            <ul>
+                <li className= {styles.title} > <Link to = "/">myMoney</Link></li>
+                <li> <Link to="/login"> Login</Link></li>
+                <li> <Link to="/signup"> Signup </Link></li>
+            </ul>
+        </nav>
+     );
+}
+ 
+export default Navbar;
