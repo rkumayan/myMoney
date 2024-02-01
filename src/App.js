dee004235fd96783c5fd193dcff3a70ce48e0476
rkumayan <kumayanrahul01@gmail.com>
2024-02-01 18:11:14 +0530

    added routes for home, login , signup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
 import './App.css';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <BrowserRouter>
+
+        <Routes>
+          <Route path = "/" element = {<Home/>}></Route>
+          <Route path = "/login" element = {<Login/>}></Route>
+          <Route path = "/signup" element = {<Signup/>}></Route>
+        </Routes>
+      </BrowserRouter>      
     </div>
   );
 }
