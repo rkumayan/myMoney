b0fbf914b50cc56166ce6af6ee69ec9aef4ff6db
rkumayan <kumayanrahul01@gmail.com>
2024-02-07 18:22:42 +0530

    added useLogout call in logout component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
 import styles from './Navbar.module.css';
 import { Link } from 'react-router-dom';
+import {useLogout} from '../hooks/useLogout';
+
 const Navbar = () => {
+    const {logout, error , isPending} = useLogout();
     return ( 
         <nav className={ styles.navbar}>
             <ul>
                 <li className= {styles.title} > <Link to = "/">myMoney</Link></li>
                 <li> <Link to="/login"> Login</Link></li>
                 <li> <Link to="/signup"> Signup </Link></li>
+                <li>
+                    <button className="btn" onClick={logout}>Logout</button>
+                </li>
             </ul>
         </nav>
      );
