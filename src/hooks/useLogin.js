cf31d04b79a7a2941fae00cc2218d23c0be46717
rkumayan <kumayanrahul01@gmail.com>
2024-02-10 18:13:06 +0530

    removed cleanup functions from useLoguot and useSignup

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import { useState} from 'react';
 import { projectAuth } from '../firebase/config';
 import { useAuthContext } from './useAuthContext';
 
@@ -6,39 +6,28 @@ export const useLogin = () =>{
     const [error, setError] = useState(null);
     const [isPending, setIsPending] = useState(false);
     const {dispatch} = useAuthContext();
-    // when component is unmounted
-    const [isCancelled, setIsCancelled] = useState(false);
+    
 
     const login = async (email, password) =>{
         setError(null);
         setIsPending(true);
 
-        // sign the user out
+        // Login the user
         try{
-            await projectAuth.signInWithEmailAndPassword(email, password);
+            const res = await projectAuth.signInWithEmailAndPassword(email, password);
             dispatch( {type : 'LOGIN' , payload : res.user } )
 
-            // when component is unmounted, don't update the states
-            if( !isCancelled){
-                setIsPending(false);
-                setError(null);
-            }            
+            setIsPending(false);
+            setError(null);                    
         }
         catch( err){
-            console.log( err.message);
-            if( !isCancelled){
-                setError( err.message);
-                setIsPending(false);
-            }            
+            console.log( err.message);        
+            setError( err.message);
+            setIsPending(false);                    
         }
     }
 
 
-    useEffect( ()=>{
-        // when component is unmounted, don't update the states
-        return () =>{
-            setIsCancelled(true);
-        }
-    }, []);
+    
     return {login, error, isPending}
 }
--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,12 +1,11 @@
-import {useEffect, useState} from 'react';
+import { useState} from 'react';
 import { projectAuth } from '../firebase/config';
 import { useAuthContext } from './useAuthContext';
 export const useLogout = () =>{
     const [error, setError] = useState(null);
     const [isPending, setIsPending] = useState(false);
     const {dispatch} = useAuthContext();
-    // when component is unmounted
-    const [isCancelled, setIsCancelled] = useState(false);
+    
     const logout = async () =>{
         setError(null);
         setIsPending(true);
@@ -16,27 +15,20 @@ export const useLogout = () =>{
             await projectAuth.signOut();
             dispatch( {type : 'LOGOUT'})
 
-            // when component is unmounted, don't update the states
-            if( !isCancelled){
-                setIsPending(false);
-                setError(null);
-            }            
+            setIsPending(false);
+            setError(null);
+                        
         }
         catch( err){
             console.log( err.message);
-            if( !isCancelled){
-                setError( err.message);
-                setIsPending(false);
-            }            
+            
+            setError( err.message);
+            setIsPending(false);
+                        
         }
     }
 
 
-    useEffect( ()=>{
-        // when component is unmounted, don't update the states
-        return () =>{
-            setIsCancelled(true);
-        }
-    }, []);
+    
     return {logout, error, isPending}
 }
